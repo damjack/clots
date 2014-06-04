d9ef9ee9dfa34e62ae8f9b52ba0f1362add29d4a
Jacob Green <jacob@nationbuilder.com>
2014-06-04 17:30:13 -0400

    Revert "Fix usage of https forms"
    This reverts commit 61fd3329923e1818b26fd613a469e21a0fcab980.

--- lib/clot/form_for.rb
+++ lib/clot/form_for.rb
@@ -146,10 +146,14 @@ module Clot
         @activity = @attributes["post_method"]
       end
 
-      if @tag_name == "secure_form_for" || FeatureFlag.https_forms?
+      if @tag_name == "secure_form_for"
         uri = URI.parse @form_action
-        uri.host = context['site'].nbuild_url
-        uri.scheme = (Rails.env.production? || Rails.env.staging?) ? "https" : "http"
+        uri.host   = context['site'].nbuild_domain
+        if Rails.env.production? || Rails.env.staging?
+          uri.scheme = "https"
+        else
+          uri.scheme = "https"
+        end
         @form_action = uri.to_s
       end
 
